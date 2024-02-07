514d353c742e24b4fb25cf94717d835c140b8d94
Noyan <143490062+NoyanBanerjee@users.noreply.github.com>
2024-02-08 01:56:21 +0530

    Update pmfilter.py

--- plugins/pmfilter.py
+++ plugins/pmfilter.py
@@ -1641,7 +1641,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         
         repluseup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
-            text=script.PLAN_TXT.format(query.from_user.mention),
+            text=script.PLAN1_TXT.format(query.from_user.mention),
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
         )
